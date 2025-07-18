89382a122c63ddba909477aec8f629709397c4b1
Riley <riley.truran@outlook.com>
2025-07-19 00:38:55 +0800

    Prepared for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,19 +4,23 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Adjust this to the frontend URL
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // frontend URL
+    })
+  );
+}
 
 app.use(rateLimiter);
 
@@ -26,6 +30,14 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
